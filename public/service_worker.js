682da8a4083acfe57ed9c0e6036b480195227e51
kerochelo <kerochelo@gmail.com>
2023-08-04 17:23:59 +0900

    :+1:setup pwa files

---
+++ public/service_worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'weather_app_cache';
+const urlsToCache = [
+  '/',
+  '/forecast',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
